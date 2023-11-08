ba89f90a9e8d7afb32bb20707d24233ad2da18de
TSE HOU <30869335+wqqhou@users.noreply.github.com>
2023-11-08 23:15:14 +0800

    Add files via upload

---
+++ apy.py
@@ -0,0 +1,8 @@
+import math
+#Convert APY (calculated with the assumption of continious compound) into simple daily interest and actual return 
+APY = user_input = float(input("Enter APY in %: "))
+P = user_input = float(input("Enter Principal: "))
+t = user_input = float(input("Enter the time to maturity in days: "))/365
+r = math.log((APY/100)+1)
+print(f'Simple daily interest is: {r/365}')
+print(f'Real return will be: {P*math.exp(r*t)-P}')
